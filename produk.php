e5aa87092e247e9d8c52e83b637ae29fb5a715eb
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-01 16:20:38 +0700

    Menambahkan produk2 yaitu game

---
+++ produk.php
@@ -0,0 +1,15 @@
+<?php 
+class Produk {
+	public 	$judul,
+			$penulis,
+			$penerbit,
+			$harga;
+}
+
+$produk1 = new produk();
+$produk1-> judul = "Naruto";
+var_dump($produk1);
+
+$produk2 = new produk();
+$produk-> judul = "Code Vein";
+ ?>}
