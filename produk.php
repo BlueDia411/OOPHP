91509939e77686e8ae97918ce2cd19a2dffc55d0
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-01 16:26:13 +0700

    menulis naruto sebagai produk 1

--- produk.php
+++ produk.php
@@ -7,9 +7,14 @@ class Produk {
 }
 
 $produk1 = new produk();
-$produk1-> judul = "Naruto";
+$produk1->judul = "Naruto";
+$produk1->penulis = "Masashi Kishimoto";
+$produk1->penerbit = "Shonen Jump";
+$produk1->harga = 25000; 
 var_dump($produk1);
 
-$produk2 = new produk();
-$produk-> judul = "Code Vein";
+// $produk2 = new produk();
+// $produk-> judul = "Code Vein";
+// $produk2-> tambahProperty = "hahaha";
+// var_dump($produk2);
  ?>}
