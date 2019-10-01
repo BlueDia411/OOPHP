1469e9247b4d83314728f0bcbf809d2ce67f340a
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-01 17:24:05 +0700

    mengganti menjadi consturctor

--- constructor.php
+++ constructor.php
@@ -23,4 +23,4 @@ $produk2 = new produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
 echo "Game : " . $produk2->getLabel();
- ?>}
+ ?>
