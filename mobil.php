7c91abfc24881bb50c300d4a604c01430c9522df
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-01 16:07:44 +0700

    Membuat PHP Produk

---
+++ mobil.php
@@ -0,0 +1,21 @@
+<?php 
+class Mobil {
+	public $nama,
+	public $merk,
+	public $warna,
+	public $kecepatanMasimal,
+	public $jumlahPenumpang;
+
+	public function tambahKecepatan(){
+
+	}
+
+	public function kurangiKecepatan(){
+
+	}
+
+	public function gantiTransisi(){
+		
+	}
+} 
+?>
